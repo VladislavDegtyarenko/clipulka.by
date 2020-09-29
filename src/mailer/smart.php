e5e81121cf9d18c3dec8a4cbc343b598931f7724
VladislavDegtyarenko <vladislavdegtyarenko@gmail.com>
2020-09-29 17:37:06 +0300

    mailer

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -1,8 +1,13 @@
 <?php 
 
-$name = $_POST['name'];
-$phone = $_POST['phone'];
+$fname = $_POST['fname'];
+$mname = $_POST['mname'];
+$lname = $_POST['lname'];
+$phone = $_POST['telephone'];
 $email = $_POST['email'];
+$city = $_POST['city'];
+$address = $_POST['address'];
+$zip = $_POST['zip'];
 
 require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
@@ -13,13 +18,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = '';                 // Наш логин
-$mail->Password = '';                           // Наш пароль от ящика
+$mail->Username = '';                 						// Ваш логин от почты с которой будут отправляться письма
+$mail->Password = '';                           		// Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('', 'Pulse');   // От кого письмо 
-$mail->addAddress('');     // Add a recipient
+$mail->setFrom('', 'Pulse');   // От кого будет уходить письмо
+$mail->addAddress('');     // Кому будет уходить письмо
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
@@ -30,10 +35,17 @@ $mail->isHTML(true);                                  // Set email format to HTM
 
 $mail->Subject = 'Данные';
 $mail->Body    = '
-		Пользователь оставил данные <br> 
-	Имя: ' . $name . ' <br>
+		Пользователь сделал заявку <br> 
+	Фамилия: ' . $lname . ' <br>
+	Имя: ' . $fname . ' <br>
+	Отчество: ' . $fname . ' <br>
 	Номер телефона: ' . $phone . '<br>
-	E-mail: ' . $email . '';
+	E-mail: ' . $email . '<br><br>
+	Адрес доставки: <br>
+	' . $city . '<br>
+	' . $address . '<br>
+	' . $zip . '<br>
+	';
 
 if(!$mail->send()) {
     return false;
